f475dae1540060036b31f36960bf6fbca9fd20a9
Anthony Larson <antlarson@yahoo.com>
2019-06-13 23:13:39 -0400

    almost done with backend

---
+++ git_project_assignment/great_ideas.sql
@@ -0,0 +1,1 @@
+SELECT * FROM great_ideas.ideas;
--- git_project_assignment/server.py
+++ git_project_assignment/server.py
@@ -27,7 +27,7 @@ def add_user_to_db():
     is_valid = True
     if len(request.form['first_name']) <= 1:
         is_valid = False
-        flash("Please enter a FIRST NAME")
+        flash("Please enter a FIRST NAME") # maybe replace flashes with Ajax/jQuery if time allows
     if len(request.form['last_name']) <= 1:
         is_valid = False
         flash("Please enter a LAST NAME")
@@ -45,12 +45,13 @@ def add_user_to_db():
         flash("Passwords do not match!")
 
     if is_valid:
-        mysql = connectToMySQL('quotes_dash2')
+        mysql = connectToMySQL('great_ideas')
         pw_hash = bcrypt.generate_password_hash(request.form['password1'])
-        query = 'INSERT INTO users (first_name, last_name, email, password) VALUES (%(fnm)s, %(lnm)s, %(eml)s, %(psw)s);'
+        query = 'INSERT INTO users (first_name, last_name, alias, email, password) VALUES (%(fnm)s, %(lnm)s, %(als)s, %(eml)s, %(psw)s);'
         data = {
             "fnm": request.form['first_name'],
             "lnm": request.form['last_name'],
+            "als": request.form['alias'],
             "eml": request.form['email'],
             "psw": pw_hash
         }
@@ -61,7 +62,7 @@ def add_user_to_db():
 
         # below we are doing another query for users with this email, and we are storing its id as session['userid']
 
-        mysql2 = connectToMySQL('quotes_dash2')
+        mysql2 = connectToMySQL('great_ideas')
         query2 = "SELECT * FROM users WHERE email = %(email2)s;"
         data2 = {
             "email2": request.form["email"]
@@ -76,7 +77,7 @@ def add_user_to_db():
 
 @app.route('/users/login', methods=['POST'])
 def login():
-    mysql = connectToMySQL('quotes_dash2')
+    mysql = connectToMySQL('great_ideas')
     query = 'SELECT * FROM users WHERE email = %(eml)s;'
     data = {
         "eml": request.form['logemail']
@@ -111,7 +112,7 @@ def welcome():
         flash("You're not logged in. Please login or register")
         return redirect('/')
     else:
-        mysql = connectToMySQL('quotes_dash2')
+        mysql = connectToMySQL('great_ideas')
         query = "SELECT * FROM users WHERE id = %(seid)s;"
         data = {
             "seid": session['userid']
@@ -121,12 +122,12 @@ def welcome():
         resulting_user = mysql.query_db(query, data)
         
 
-        mysql2 = connectToMySQL('quotes_dash2')
-        quoteText = "SELECT * FROM users JOIN quotes ON users.id = quotes.user_id WHERE users.id ORDER BY quotes.id DESC;"
+        mysql2 = connectToMySQL('great_ideas')
+        ideaText = "SELECT * FROM users JOIN ideas ON users.id = ideas.user_id WHERE users.id ORDER BY ideas.id DESC;"
         # quoteText = "SELECT * FROM quotes LEFT JOIN users ON quotes.user_id = users.id ORDER BY quotes.id DESC;"
         
         # resultQuote has all the information of users and quotes tied together via users.id = quotes.user_id
-        resultQuote = mysql2.query_db(quoteText)
+        resultIdea = mysql2.query_db(ideaText)
 
         
 
@@ -137,16 +138,19 @@ def welcome():
         #     "_id": session['userid']
         # }
 
-        mysql3 = connectToMySQL('quotes_dash2')
-        quoteText2 = "SELECT * FROM users JOIN quotes ON users.id=quotes.user_id JOIN likes ON quotes.id=likes.quote_id WHERE users.id = likes.user_id ORDER BY quotes.id DESC;"
+        mysql3 = connectToMySQL('great_ideas')
+        ideaText2 = "SELECT * FROM users JOIN ideas ON users.id=ideas.user_id JOIN likes ON ideas.id=likes.quote_id WHERE users.id = likes.user_id ORDER BY ideas.id DESC;"
         data2 = {
             "_id": session['userid']
         }
 
-        mysql3.query_db(quoteText2, data2)
+        mysql3.query_db(ideaText2, data2)
 
 
-        return render_template("welcome.html", result_user=resulting_user[0], resultQuotes = resultQuote)
+        return render_template("welcome.html", result_user=resulting_user[0], resultIdeas = resultIdea)
+
+
+
 
 
 @app.route('/users/logout', methods=['GET'])
@@ -158,25 +162,31 @@ def logout():
 
 
 
-@app.route('/quotes/create', methods=['POST'])
-def write_quote():
+
+
+'''
++++++++++++++++++++++++
+'''
+
+
+
+
+
+
+@app.route('/ideas/create', methods=['POST'])
+def write_idea():
     is_valid = True
-    if len(request.form['author_name']) < 3:
+    if len(request.form['idea_content']) < 10:
         is_valid = False
-        flash("The author's name must be 3 characters or more.")
-        return redirect('/users/welcome')
-    if len(request.form['quote_content']) < 10:
-        is_valid = False
-        flash("The quote must be at least 10 characters long.")
+        flash("The idea must be at least 10 characters long.")
         return redirect('/users/welcome')
 
     if is_valid:
-        mysql = connectToMySQL('quotes_dash2')
-        query = 'INSERT INTO quotes (user_id, author, quote_content) VALUES (%(userid)s, %(author)s, %(quote)s);'
+        mysql = connectToMySQL('great_ideas')
+        query = 'INSERT INTO ideas (user_id, idea_content) VALUES (%(userid)s, %(idea)s);'
         data = {
             "userid": session['userid'],
-            "author": request.form['author_name'],
-            "quote": request.form['quote_content']
+            "idea": request.form['idea_content']
         }
         mysql.query_db(query, data)
         return redirect('/users/welcome')
@@ -187,18 +197,18 @@ def write_quote():
 # I'm going to have to figure out how to include DELETE only on the posts that are associated with that user
 # Maybe use an if statement in the template itself
 
-@app.route('/quotes/<id>/delete', methods=['POST'])
-def delete_quote(id):
+@app.route('/ideas/<id>/delete', methods=['POST'])
+def delete_idea(id):
     
-    mysql2 = connectToMySQL('quotes_dash2')
-    query2 = 'DELETE FROM likes WHERE quote_id = %(q_id)s'
+    mysql2 = connectToMySQL('great_ideas')
+    query2 = 'DELETE FROM likes WHERE idea_id = %(q_id)s'
     data2 = {
         "q_id": id
     }
     mysql2.query_db(query2, data2)
 
-    mysql = connectToMySQL('quotes_dash2')
-    query = 'DELETE FROM quotes WHERE id = %(_id)s;'
+    mysql = connectToMySQL('great_ideas')
+    query = 'DELETE FROM ideas WHERE id = %(_id)s;'
     data = {
         "_id": id
     }
@@ -219,11 +229,11 @@ def delete_quote(id):
 
 
 
-@app.route('/quotes/<id>/add_like', methods=['POST'])
+@app.route('/ideas/<id>/add_like', methods=['POST'])
 def add_like(id):
     is_valid = True
-    mysql2 = connectToMySQL('quotes_dash2')
-    query2 = 'SELECT * FROM likes WHERE user_id = %(us_id)s AND quote_id = %(qu_id)s;'
+    mysql2 = connectToMySQL('great_ideas')
+    query2 = 'SELECT * FROM likes WHERE user_id = %(us_id)s AND idea_id = %(qu_id)s;'
     data2 = {
         "us_id": request.form["userLike"],
         "qu_id": id
@@ -232,8 +242,8 @@ def add_like(id):
 
     if result2:
         is_valid = False
-        mysql3 = connectToMySQL('quotes_dash2')
-        query3 = 'DELETE FROM likes WHERE quote_id = %(qid)s AND user_id = %(uid)s;'
+        mysql3 = connectToMySQL('great_ideas')
+        query3 = 'DELETE FROM likes WHERE idea_id = %(qid)s AND user_id = %(uid)s;'
         data3 = {
             "qid": id,
             "uid": request.form['userLike']
@@ -243,14 +253,14 @@ def add_like(id):
     # else:
     #     is_valid = True
     if is_valid:
-        mysql = connectToMySQL('quotes_dash2')
-        query = 'INSERT INTO likes (quote_id, user_id) VALUES (%(qid)s, %(uid)s);'
+        mysql = connectToMySQL('great_ideas')
+        query = 'INSERT INTO likes (idea_id, user_id) VALUES (%(qid)s, %(uid)s);'
         data = {
             "qid": id,
             "uid": request.form['userLike']
         }
         mysql.query_db(query, data)
-        flash("You liked this quote!")
+        flash("You liked this idea.")
 
     # mysql4 = connectToMySQL('quotes_dash2')
     # query4 = "SELECT COUNT(user_id) as Those_Who_Like FROM likes WHERE quote_id = %(q_id)s;"
@@ -271,14 +281,14 @@ def add_like(id):
 
 
 
-@app.route('/users/<id>/view')
-def view_user(id):
-    mysql = connectToMySQL('quotes_dash2')
-    query = "SELECT * FROM quotes WHERE user_id = %(q_id)s;"
+@app.route('/users/<id>/view_author')
+def view_author(id):
+    mysql = connectToMySQL('great_ideas')
+    query = "SELECT * FROM ideas WHERE user_id = %(q_id)s;"
     data = {
         "q_id": id
     }
-    userQuotes = mysql.query_db(query, data)
+    userIdeas = mysql.query_db(query, data)
 
     # mysql2 = connectToMySQL('quotes_dash2')
     # query2 = "SELECT * FROM users WHERE id = %(_us_id)s;"
@@ -289,20 +299,22 @@ def view_user(id):
     # ourUser = mysql2.query_db(query2, data2)
     # first = ourUser['first_name']
     # second = ourUser['last_name']
-    if len(userQuotes) > 0:
-        first = userQuotes[0]['user_id']
 
-    mysql2 = connectToMySQL('quotes_dash2')
+    if len(userIdeas) > 0:
+        first = userIdeas[0]['user_id']
+
+    mysql2 = connectToMySQL('great_ideas')
     query2 = 'SELECT * FROM users WHERE id = %(u_id)s;'
     data2 = {
         "u_id": first
     }
     user = mysql2.query_db(query2, data2)
     if len(user) > 0:
-        user1 = user[0]['first_name']
-        user2 = user[0]['last_name']
+        user = user[0]
+        # user1 = user[0]['first_name']
+        # user2 = user[0]['last_name']
 
-    return render_template('view.html', userQuotes= userQuotes, first= user1, last=user2)
+    return render_template('view_author.html', userQuotes= userIdeas, user = user)
 
 
 
@@ -329,56 +341,56 @@ def view_user(id):
 
 
 
-@app.route('/users/<id>/edit', methods=['GET'])
-def edit_user(id):
-    mysql = connectToMySQL('quotes_dash2')
-    query = 'SELECT * FROM users WHERE id = %(_id)s;'
-    data = {
-        "_id": id
-    }
-    user = mysql.query_db(query, data)
-    if len(user) > 0:
-        user = user[0]
+# @app.route('/users/<id>/edit', methods=['GET'])
+# def edit_user(id):
+#     mysql = connectToMySQL('quotes_dash2')
+#     query = 'SELECT * FROM users WHERE id = %(_id)s;'
+#     data = {
+#         "_id": id
+#     }
+#     user = mysql.query_db(query, data)
+#     if len(user) > 0:
+#         user = user[0]
 
-    return render_template('edit.html', result = user)
+#     return render_template('edit.html', result = user)
 
 
 
-@app.route('/quotes/<id>/update', methods=['POST'])
-def update_tweet(id):
-    mysql2 = connectToMySQL('quotes_dash2')
-    query2 = 'SELECT * FROM users WHERE id = %(_id2)s;'
-    data2 = {
-        "_id2": session['userid']
-    }
-    user2 = mysql2.query_db(query2, data2)
-    if len(user2) > 0:
-        user2 = user2[0]
+# @app.route('/quotes/<id>/update', methods=['POST'])
+# def update_tweet(id):
+#     mysql2 = connectToMySQL('quotes_dash2')
+#     query2 = 'SELECT * FROM users WHERE id = %(_id2)s;'
+#     data2 = {
+#         "_id2": session['userid']
+#     }
+#     user2 = mysql2.query_db(query2, data2)
+#     if len(user2) > 0:
+#         user2 = user2[0]
 
-    is_valid = True
-    var1 = request.form['email'] # the email they wrote
-    var2 = user2['email']
-    if var1 == var2:
-        is_valid = False
-        flash("Please use a different email address.")
-        return redirect('/users/<id>/edit')
-    if not EMAIL_REGEX.match(request.form['email']):
-        is_valid = False
-        flash("Please use correct email formatting.")
-        return redirect('/users/<id>/edit')
+#     is_valid = True
+#     var1 = request.form['email'] # the email they wrote
+#     var2 = user2['email']
+#     if var1 == var2:
+#         is_valid = False
+#         flash("Please use a different email address.")
+#         return redirect('/users/<id>/edit')
+#     if not EMAIL_REGEX.match(request.form['email']):
+#         is_valid = False
+#         flash("Please use correct email formatting.")
+#         return redirect('/users/<id>/edit')
 
-    if is_valid:
-        mysql = connectToMySQL('quotes_dash2')
-        query = 'UPDATE users SET first_name = %(first)s, last_name = %(last)s, email = %(email)s WHERE id = %(_id)s;'
-        data = {
-            "first": request.form['first_name'],
-            "last": request.form['last_name'],
-            "email": request.form['email'],
-            "_id": id
-        }
-        mysql.query_db(query, data)
-        flash("Thank you for updating your information.")
-        return redirect ('/users/welcome')
+#     if is_valid:
+#         mysql = connectToMySQL('quotes_dash2')
+#         query = 'UPDATE users SET first_name = %(first)s, last_name = %(last)s, email = %(email)s WHERE id = %(_id)s;'
+#         data = {
+#             "first": request.form['first_name'],
+#             "last": request.form['last_name'],
+#             "email": request.form['email'],
+#             "_id": id
+#         }
+#         mysql.query_db(query, data)
+#         flash("Thank you for updating your information.")
+#         return redirect ('/users/welcome')
 
 
 
